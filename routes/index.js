13fd0c4b7fc7459116dfb7b1b246f72261a311af
sneha <singh1688@gmail.com>
2013-08-15 18:41:30 -0700

    Added functionality for counting the number of responses

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var db = redis.createClient();
 exports.index = function(req, res){
 
 	//db.mset(["Q1", "0", "Q2", "0"], function (err, res) {});
-    db.mset(["op1","0","op2","0","op3","0"], function (err, res) {});
+    //db.mset(["op1","0","op2","0","op3","0"], function (err, res) {});
     //db.get("test keys 1", function (err, reply) {
     //    console.log(reply.toString());
     //});
--- routes/submission.js
+++ routes/submission.js
@@ -7,11 +7,41 @@ exports.submit = function(req, res){
         console.log(reply.toString());
   });*/
   var radio = req.param('q1radio');
+  var option1Count = 0;
   if(radio == "option1")
   {
-  	console.log("bingo");
+  	/*
+  	db.get("op1", function (err, reply) {
+  		
+  		option1Count = parseInt(reply) + 1;
+        console.log(option1Count);
+    });
+    db.set("op1",option1Count.toString());
+    */
+    db.incr("op1");
+   	db.get("op1", function (err, reply) {
+        console.log(reply.toString());
+    });
+  	console.log("DEBUG: option1 selected");
   }
-  console.log(req.param('q1radio'));
+  else if(radio == "option2")
+  {
+    db.incr("op2");
+   	db.get("op2", function (err, reply) {
+        console.log(reply.toString());
+    });
+  	console.log("DEBUG: option2 selected");
+  }
+  else if(radio == "option3")
+  {
+    db.incr("op3");
+   	db.get("op3", function (err, reply) {
+        console.log(reply.toString());
+    });
+  	console.log("DEBUG: option3 selected");
+  }
+
+  //console.log(req.param('q1radio'));
 
    
   res.send("This is the submission");
