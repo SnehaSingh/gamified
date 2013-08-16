9715638430debff01aff22eb411406939b973ec9
sneha <singh1688@gmail.com>
2013-08-15 23:30:52 -0700

    configuring rediscloud on index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@
  */
 
 var redis = require('redis');
-var db = redis.createClient();
+var url = require('url');
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+db.auth(redisURL.auth.split(":")[1]);
 
 exports.index = function(req, res){
 
